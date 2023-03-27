efe23fb230508b7d871fee267fd69c9890a0ca22
summer-o3o <ho_ho95@naver.com>
2023-03-27 19:58:31 +0900

    add : 모달 추가

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Common from '../../components/Common/Common';
+import Modal from '../../components/Modal/Modal';
 
 const Main = () => {
-  const { IcoMovie } = Common;
-
   return (
-    <IcoMovie width="29px" height="27px" backgroundPosition="-100px 0">
-      dddd
-    </IcoMovie>
+    <Modal
+      title="로그인 팝업"
+      contents={`로그인이 필요한 서비스 입니다. \n 로그인페이지로 이동하시겠습니까?`}
+      btnContents={['취소', '확인']}
+    />
   );
 };
 
