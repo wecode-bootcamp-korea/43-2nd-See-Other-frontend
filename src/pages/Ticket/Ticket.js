5403225dbacbe8bdf6cf2470e11615b992ddc616
이이솜이 <somyi-lee@iisom-is-MacBook-Air.local>
2023-03-27 23:52:07 +0900

    ADD : Theater layout -MODIFY: theater layout 최종 구현 -MODIFY: nesting delete -MODIFY : theater 기능 초기세팅 -ADD : 영화 옵션 선택 및 정보 저장 기능 구현 -ADD : QUICK MENU 정보 변경 기능 구현 -MODIFY: selectd state, selectInfo 초기값 수정

--- src/pages/Ticket/Ticket.js
+++ src/pages/Ticket/Ticket.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import Seat from './Seat/Seat';
 import QuickMenu from '../QuickMenu/QuickMenu';
+import Theater from './Component/Theater/Theater';
+
+const initValue = {
+  name: '',
+  grade: '',
+  region: '',
+  cinema: '',
+  theater: '',
+  date: '',
+  time: '',
+  seat: [],
+};
 
 const Ticket = () => {
+  const [selectInfo, setSelectInfo] = useState(initValue);
+  const handleValue = (key, value) => {
+    setSelectInfo(prevState => ({
+      ...prevState,
+      [key]: value,
+    }));
+  };
+
   return (
-    <TicketWrap>
-      <Seat />
-      <QuickMenu />
-    </TicketWrap>
+    <>
+      <TicketWrap>
+        <Theater handleValue={handleValue} selectInfo={selectInfo} />
+      </TicketWrap>
+      <QuickMenu {...selectInfo} />
+    </>
   );
 };
+
+export default Ticket;
 
 const TicketWrap = styled.div`
   background-color: #f2f0e4;
@@ -18,4 +41,3 @@ const TicketWrap = styled.div`
   height: 600px;
   margin: 0 auto;
 `;
-export default Ticket;
